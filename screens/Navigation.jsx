80b7f85edb26f7f264561e8471ee45ed16fc7c1b
Grom93 <38588912+Grom93@users.noreply.github.com>
2023-07-16 01:21:04 +0300

    ReactNativ

---
+++ screens/Navigation.jsx
@@ -0,0 +1,19 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { FullPostScreen } from './FullPost';
+import { HomeScreen } from './Home';
+
+const Stack = createNativeStackNavigator();
+
+// <Routes>....</Routes> => Stack.Navigator
+
+export const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Начальная' }} />
+        <Stack.Screen name="FullPost" component={FullPostScreen} options={{ title: 'Товар' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
